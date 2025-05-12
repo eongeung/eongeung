0eb3e22c2e226908b50f0ad9f51758a5cda596e4
이영흔 <yeongheun@iyeongheun-ui-MacBookAir.local>
2025-05-12 16:08:30 +0900

    feat : Add click

--- js/include.js
+++ js/include.js
@@ -1,14 +1,30 @@
 window.addEventListener('load', function () {
   const allElements = document.getElementsByTagName('*');
+  let includeCount = 0;
+  let totalIncludes = 0;
+
   Array.prototype.forEach.call(allElements, function (el) {
     const includePath = el.dataset.includePath;
     if (includePath) {
+      totalIncludes++;
+
       const xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
           el.outerHTML = this.responseText;
+          includeCount++;
+
+          if (includeCount === totalIncludes && window.location.hash) {
+            const target = document.querySelector(window.location.hash);
+            if (target) {
+              setTimeout(() => {
+                target.scrollIntoView({ behavior: 'smooth' });
+              }, 100);
+            }
+          }
         }
       };
+
       xhttp.open('GET', includePath, true);
       xhttp.send();
     }
