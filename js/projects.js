d1a78ec035c5b0eacb7193ffd607027a800168f3
이영흔 <yeongheun@iyeongheun-ui-MacBookAir.local>
2025-05-12 15:40:31 +0900

    test : Add test code

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,6 @@
 function toggle(element) {
+  console.log("클릭됨", element);
+  
   const desc = element.querySelector('.description');
   const isOpen = desc.style.display === 'block';
   document.querySelectorAll('.description').forEach(d => d.style.display = 'none');
