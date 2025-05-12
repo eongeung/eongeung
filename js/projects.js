8cac7f5542db0bfa88c35df4d8385c38a0d88a88
이영흔 <yeongheun@iyeongheun-ui-MacBookAir.local>
2025-05-12 16:01:03 +0900

    style : Modify style

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,4 @@
 function toggle(element) {
-  console.log("클릭됨", element);
-  
   const desc = element.querySelector('.description');
   const isOpen = desc.style.display === 'block';
   document.querySelectorAll('.description').forEach(d => d.style.display = 'none');
