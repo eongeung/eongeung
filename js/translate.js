224c656698d46e5148a159e86f3b702371f52c18
yeongheun <20222104@gm.hannam.ac.kr>
2025-05-14 05:41:26 +0900

    style : Modify css

--- js/translate.js
+++ js/translate.js
@@ -1,22 +1,25 @@
 window.addEventListener("includesLoaded", function () {
-  const btn = document.getElementById("langToggle");
-  const ko = document.querySelector('[id$="-ko"]');
-  const en = document.querySelector('[id$="-en"]');
+  const allToggleButtons = document.querySelectorAll("#langToggle");
 
-  if (!btn || !ko || !en) return;
+  allToggleButtons.forEach((btn) => {
+    const container = btn.closest("section") || document;
+    const ko = container.querySelector('[id$="-ko"]');
+    const en = container.querySelector('[id$="-en"]');
 
+    if (!btn || !ko || !en) return;
 
-  btn.addEventListener("click", () => {
-    const isKoVisible = ko.style.display !== "none";
+    btn.addEventListener("click", () => {
+      const isKoVisible = ko.style.display !== "none";
 
-    if (isKoVisible) {
-      ko.style.display = "none";
-      en.style.display = "block";
-      btn.textContent = "KR 🇰🇷";
-    } else {
-      ko.style.display = "block";
-      en.style.display = "none";
-      btn.textContent = "EN 🇺🇸";
-    }
+      if (isKoVisible) {
+        ko.style.display = "none";
+        en.style.display = "block";
+        btn.textContent = "KR 🇰🇷";
+      } else {
+        ko.style.display = "block";
+        en.style.display = "none";
+        btn.textContent = "EN 🇺🇸";
+      }
+    });
   });
 });
