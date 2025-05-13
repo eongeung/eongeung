3484fec9865d9425b93c676be5106581ed5cd83c
yeongheun <20222104@gm.hannam.ac.kr>
2025-05-14 04:24:50 +0900

    style : Modify css

--- js/translate.js
+++ js/translate.js
@@ -1,17 +1,19 @@
-const btn = document.getElementById("langToggle");
-const ko = document.getElementById("about-ko");
-const en = document.getElementById("about-en");
+window.onload = function () {
+  const btn = document.getElementById("langToggle");
+  const ko = document.getElementById("about-ko");
+  const en = document.getElementById("about-en");
 
-btn.addEventListener("click", () => {
-  const isKoVisible = ko.style.display !== "none";
+  btn.addEventListener("click", () => {
+    const isKoVisible = ko.style.display !== "none";
 
-  if (isKoVisible) {
-    ko.style.display = "none";
-    en.style.display = "block";
-    btn.textContent = "KR";
-  } else {
-    ko.style.display = "block";
-    en.style.display = "none";
-    btn.textContent = "EN";
-  }
-});
+    if (isKoVisible) {
+      ko.style.display = "none";
+      en.style.display = "block";
+      btn.textContent = "KR";
+    } else {
+      ko.style.display = "block";
+      en.style.display = "none";
+      btn.textContent = "EN";
+    }
+  });
+};
