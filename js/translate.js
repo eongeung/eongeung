6f086ead67d02f260d59c4287ae1ada099e47da5
yeongheun <20222104@gm.hannam.ac.kr>
2025-05-14 04:17:33 +0900

    style : Modify css

--- js/translate.js
+++ js/translate.js
@@ -1,21 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("langToggle");
-  const ko = document.getElementById("about-ko");
-  const en = document.getElementById("about-en");
+const btn = document.getElementById("langToggle");
+const ko = document.getElementById("about-ko");
+const en = document.getElementById("about-en");
 
-  if (!btn || !ko || !en) return;
-  
-  btn.addEventListener("click", () => {
-    const isKoVisible = ko.style.display !== "none";
+btn.addEventListener("click", () => {
+  const isKoVisible = ko.style.display !== "none";
 
-    if (isKoVisible) {
-      ko.style.display = "none";
-      en.style.display = "block";
-      btn.textContent = "KR 🇰🇷";
-    } else {
-      ko.style.display = "block";
-      en.style.display = "none";
-      btn.textContent = "EN 🇺🇸";
-    }
-  });
+  if (isKoVisible) {
+    ko.style.display = "none";
+    en.style.display = "block";
+    btn.textContent = "KR";
+  } else {
+    ko.style.display = "block";
+    en.style.display = "none";
+    btn.textContent = "EN";
+  }
 });
